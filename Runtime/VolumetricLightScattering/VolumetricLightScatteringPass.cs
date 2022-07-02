73dadfcb5a74f545e37835cdf976f1c11d15612a
Nixon <voxell.technologies@gmail.com>
2022-07-02 14:25:46 +0800

    wrapping classes back up into namespace

--- Runtime/VolumetricLightScattering/VolumetricLightScatteringPass.cs
+++ Runtime/VolumetricLightScattering/VolumetricLightScatteringPass.cs
@@ -3,105 +3,108 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
-internal class VolumetricLightScatteringPass : ScriptableRenderPass
+namespace Voxell.VolumetricLighting
 {
-  private ProfilingSampler _profilingSampler = new ProfilingSampler("VolumetricLightScattering");
-
-  private Material _occluderMaterial;
-  private Material _radialBlurMaterial;
-
-  private RTHandle _cameraColorTarget;
-  private RTHandle _occluderTarget;
-
-  private VolumetricLightScattering.Settings _settings;
-  private readonly List<ShaderTagId> _shaderTagIdList = new List<ShaderTagId>
+  internal class VolumetricLightScatteringPass : ScriptableRenderPass
   {
-    new ShaderTagId("UniversalForward"),
-    new ShaderTagId("UniversalForwardOnly"),
-    new ShaderTagId("LightweightForward"),
-    new ShaderTagId("SRPDefaultUnlit")
-  };
-  private FilteringSettings _filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+    private ProfilingSampler _profilingSampler = new ProfilingSampler("VolumetricLightScattering");
 
-  public VolumetricLightScatteringPass(
-    Material occludersMaterial, Material radialBlurMaterial,
-    VolumetricLightScattering.Settings settings
-  )
-  {
-    this._occluderMaterial = occludersMaterial;
-    this._radialBlurMaterial = radialBlurMaterial;
-    this._settings = settings;
-    this.renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
+    private Material _occluderMaterial;
+    private Material _radialBlurMaterial;
 
-    this._occluderTarget = RTHandles.Alloc(
-      (int)(Screen.width*_settings.resolutionScale), (int)(Screen.height*_settings.resolutionScale),
-      depthBufferBits: DepthBits.None
-    );
-  }
+    private RTHandle _cameraColorTarget;
+    private RTHandle _occluderTarget;
 
-  public void SetTarget(RTHandle colorHandle)
-  {
-    this._cameraColorTarget = colorHandle;
-  }
-
-  public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
-  {
-    // finish configuration
-    ConfigureTarget(this._occluderTarget);
-  }
-
-  public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
-  {
-    if (this._occluderMaterial == null || this._radialBlurMaterial == null) return; 
-    if (RenderSettings.sun ==  null || !RenderSettings.sun.enabled) return;
-
-    CommandBuffer cmd = CommandBufferPool.Get();
-    using (new ProfilingScope(cmd, this._profilingSampler))
+    private VolumetricLightScattering.Settings _settings;
+    private readonly List<ShaderTagId> _shaderTagIdList = new List<ShaderTagId>
     {
-      context.ExecuteCommandBuffer(cmd);
-      cmd.Clear();
+      new ShaderTagId("UniversalForward"),
+      new ShaderTagId("UniversalForwardOnly"),
+      new ShaderTagId("LightweightForward"),
+      new ShaderTagId("SRPDefaultUnlit")
+    };
+    private FilteringSettings _filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
 
-      Camera camera = renderingData.cameraData.camera;
-      context.DrawSkybox(camera);
+    public VolumetricLightScatteringPass(
+      Material occludersMaterial, Material radialBlurMaterial,
+      VolumetricLightScattering.Settings settings
+    )
+    {
+      this._occluderMaterial = occludersMaterial;
+      this._radialBlurMaterial = radialBlurMaterial;
+      this._settings = settings;
+      this.renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
 
-      DrawingSettings drawSettings = CreateDrawingSettings(
-        this._shaderTagIdList, ref renderingData, SortingCriteria.CommonOpaque
+      this._occluderTarget = RTHandles.Alloc(
+        (int)(Screen.width*_settings.resolutionScale), (int)(Screen.height*_settings.resolutionScale),
+        depthBufferBits: DepthBits.None
       );
-      drawSettings.overrideMaterial = _occluderMaterial;
-      context.DrawRenderers(renderingData.cullResults, ref drawSettings, ref this._filteringSettings);
-
-      // schedule it for execution and release it after the execution
-      context.ExecuteCommandBuffer(cmd);
-      CommandBufferPool.Release(cmd);
-
-      Vector3 sunDirectionWorldSpace = RenderSettings.sun.transform.forward;
-      Vector3 cameraDirectionWorldSpace = camera.transform.forward;
-      Vector3 cameraPositionWorldSpace = camera.transform.position;
-      Vector3 sunPositionWorldSpace = cameraPositionWorldSpace + sunDirectionWorldSpace;
-      Vector3 sunPositionViewportSpace = camera.WorldToViewportPoint(sunPositionWorldSpace);
-
-      float dotProd = Vector3.Dot(-cameraDirectionWorldSpace, sunDirectionWorldSpace);
-      dotProd -= Vector3.Dot(cameraDirectionWorldSpace, Vector3.down);
-      float intensityFader = dotProd / _settings.fadeRange;
-      intensityFader = Mathf.Clamp(intensityFader, 0.0f, 1.0f);
-
-      _radialBlurMaterial.SetColor("_Color", RenderSettings.sun.color);
-      _radialBlurMaterial.SetVector("_Center", sunPositionViewportSpace);
-      _radialBlurMaterial.SetFloat("_BlurWidth", _settings.blurWidth);
-      _radialBlurMaterial.SetFloat("_NumSamples", _settings.numSamples);
-      _radialBlurMaterial.SetFloat("_Intensity", _settings.intensity * intensityFader);
-
-      _radialBlurMaterial.SetVector("_NoiseSpeed", _settings.noiseSpeed);
-      _radialBlurMaterial.SetFloat("_NoiseScale", _settings.noiseScale);
-      _radialBlurMaterial.SetFloat("_NoiseStrength", _settings.noiseStrength);
-
-      Blit(cmd, _occluderTarget, _cameraColorTarget, _radialBlurMaterial);
     }
-  }
 
-  public override void OnCameraCleanup(CommandBuffer cmd)
-  {
-    base.OnCameraCleanup(cmd);
-    cmd.ReleaseTemporaryRT(Shader.PropertyToID(_occluderTarget.name));
+    public void SetTarget(RTHandle colorHandle)
+    {
+      this._cameraColorTarget = colorHandle;
+    }
+
+    public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
+    {
+      // finish configuration
+      ConfigureTarget(this._occluderTarget);
+    }
+
+    public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+    {
+      if (this._occluderMaterial == null || this._radialBlurMaterial == null) return; 
+      if (RenderSettings.sun ==  null || !RenderSettings.sun.enabled) return;
+
+      CommandBuffer cmd = CommandBufferPool.Get();
+      using (new ProfilingScope(cmd, this._profilingSampler))
+      {
+        context.ExecuteCommandBuffer(cmd);
+        cmd.Clear();
+
+        Camera camera = renderingData.cameraData.camera;
+        context.DrawSkybox(camera);
+
+        DrawingSettings drawSettings = CreateDrawingSettings(
+          this._shaderTagIdList, ref renderingData, SortingCriteria.CommonOpaque
+        );
+        drawSettings.overrideMaterial = _occluderMaterial;
+        context.DrawRenderers(renderingData.cullResults, ref drawSettings, ref this._filteringSettings);
+
+        // schedule it for execution and release it after the execution
+        context.ExecuteCommandBuffer(cmd);
+        CommandBufferPool.Release(cmd);
+
+        Vector3 sunDirectionWorldSpace = RenderSettings.sun.transform.forward;
+        Vector3 cameraDirectionWorldSpace = camera.transform.forward;
+        Vector3 cameraPositionWorldSpace = camera.transform.position;
+        Vector3 sunPositionWorldSpace = cameraPositionWorldSpace + sunDirectionWorldSpace;
+        Vector3 sunPositionViewportSpace = camera.WorldToViewportPoint(sunPositionWorldSpace);
+
+        float dotProd = Vector3.Dot(-cameraDirectionWorldSpace, sunDirectionWorldSpace);
+        dotProd -= Vector3.Dot(cameraDirectionWorldSpace, Vector3.down);
+        float intensityFader = dotProd / _settings.fadeRange;
+        intensityFader = Mathf.Clamp(intensityFader, 0.0f, 1.0f);
+
+        _radialBlurMaterial.SetColor("_Color", RenderSettings.sun.color);
+        _radialBlurMaterial.SetVector("_Center", sunPositionViewportSpace);
+        _radialBlurMaterial.SetFloat("_BlurWidth", _settings.blurWidth);
+        _radialBlurMaterial.SetFloat("_NumSamples", _settings.numSamples);
+        _radialBlurMaterial.SetFloat("_Intensity", _settings.intensity * intensityFader);
+
+        _radialBlurMaterial.SetVector("_NoiseSpeed", _settings.noiseSpeed);
+        _radialBlurMaterial.SetFloat("_NoiseScale", _settings.noiseScale);
+        _radialBlurMaterial.SetFloat("_NoiseStrength", _settings.noiseStrength);
+
+        Blit(cmd, _occluderTarget, _cameraColorTarget, _radialBlurMaterial);
+      }
+    }
+
+    public override void OnCameraCleanup(CommandBuffer cmd)
+    {
+      base.OnCameraCleanup(cmd);
+      cmd.ReleaseTemporaryRT(Shader.PropertyToID(_occluderTarget.name));
+    }
   }
 }
